43b10a4241a119a6e573eb81446dfe657cd2cf56
Artem <nemezg@gmail.com>
2025-07-31 16:25:25 +0300

    browse_meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,20 @@
+import Link from "next/link";
+import MealsGrid from "@/components/meals/meals-grid";
+import classes from "./page.module.css";
+
 export default function MealsPage() {
   return (
-    <div>
-      <h1>Delicious Meals</h1>
-      <p>Explore our wide variety of meals!</p>
-    </div>
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, created <span className={classes.highlight}>by YOU</span>
+        </h1>
+        <p>Craft your meal experience with our diverse options.</p>
+        <p className={classes.cta}>
+          <Link href="meals/share">Share your favorite recipes!</Link>
+        </p>
+      </header>
+      <main className={classes.main}><MealsGrid meals={[]} /></main>
+    </>
   );
 }
